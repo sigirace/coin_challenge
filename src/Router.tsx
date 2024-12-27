846bf8a062ad78f1842c5b99b5ed68f553773bb9
SIGIRACE <chickang9@gmail.com>
2024-12-27 12:48:32 +0900

    feat: project setup

---
+++ src/Router.tsx
@@ -0,0 +1,12 @@
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+
+const Router = () => {
+    return <BrowserRouter>
+        <Switch>
+            <Route path="/">
+            </Route>
+        </Switch>
+    </BrowserRouter>
+}
+
+export default Router;
---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  textColor: "#f5f6fa",
+  bgColor: "#2f3640",
+  accentColor: "#44bd32",
+};
+
+export { theme };
